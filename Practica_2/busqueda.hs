3898847ba679f58bdc9a533143ca6ad5c0c7d3e7
Yoshua_Flores <yoshuaflores@ciencias.unam.mx>
2024-09-06 10:20:23 -0600

    Ejercicio 2 - busqueda binaria

--- Practica_2/busqueda.hs
+++ Practica_2/busqueda.hs
@@ -3,9 +3,25 @@ busquedaLineal x (y:ys)
   | x == y =True
   | otherwise = busquedaLineal x ys
 
-busquedaBinaria x [] = (-1)
-busquedaBinaria x (y:ys:yss)
-  | x == ys = 1
-  | x <= ys = busquedaBinaria x y
-  | otherwise = busquedaBinaria x yss
+half xs = splitAt (length xs `div` 2) xs
+
+compareAt y x xs
+  | (xs !! y) == x = 0
+  | (xs !! y) < x = -1
+  | otherwise = 1
+
+busquedaTempLista x xs ys
+  | x == (xs !! (length xs -1)) = 0
+  | x <= (xs !! (length xs -1)) = -1  
+  | x == (ys !! (length ys -1)) = 0
+   | x > (ys !! (length ys -1)) = 1
   
+busquedaBinaria :: (Ord a) => a -> [a] -> Maybe Int
+busquedaBinaria _ [] = Nothing
+busquedaBinaria n xs
+  | elem == n = Just indice
+  | elem < n  = (+indice) . (+1) <$> busquedaBinaria n bs
+  | otherwise = busquedaBinaria n as
+  where indice = length xs `quot` 2
+        (as,elem:bs) = splitAt indice xs
+
